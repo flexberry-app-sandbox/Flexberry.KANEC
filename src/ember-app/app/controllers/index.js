d092fb71789f9feb2ee23624d50b389b78b6135b
Flexberry-man <mail@flexberry.net>
2023-12-04 08:31:11 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,37 +22,37 @@ export default Controller.extend({
             link: 'i-i-s-k-a-n-e-c-спрос-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-спрос-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-спрос-l.title'),
-            icon: 'chart line',
+            icon: 'phone',
             children: null
           }, {
             link: 'i-i-s-k-a-n-e-c-персонал-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-персонал-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-персонал-l.title'),
-            icon: 'table',
+            icon: 'folder open',
             children: null
           }, {
             link: 'i-i-s-k-a-n-e-c-маркетолог-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-маркетолог-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-маркетолог-l.title'),
-            icon: 'chart bar',
+            icon: 'list',
             children: null
           }, {
             link: 'i-i-s-k-a-n-e-c-основ-показат-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-основ-показат-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-основ-показат-l.title'),
-            icon: 'folder',
+            icon: 'chart bar',
             children: null
           }, {
             link: 'i-i-s-k-a-n-e-c-должность-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-должность-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-должность-l.title'),
-            icon: 'edit',
+            icon: 'tags',
             children: null
           }, {
             link: 'i-i-s-k-a-n-e-c-целевая-аудит-l',
             caption: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-целевая-аудит-l.caption'),
             title: i18n.t('forms.application.sitemap.k-a-n-e-c.i-i-s-k-a-n-e-c-целевая-аудит-l.title'),
-            icon: 'suitcase',
+            icon: 'list',
             children: null
           }]
         }
